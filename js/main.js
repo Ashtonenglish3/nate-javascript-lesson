774eb5b8bf171f96268d23d2b1e55b2072bc836d
Ashton <ashtonenglish3@gmail.com>
2018-11-08 12:02:43 +0200

    first task on javascript

---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+// The "ude strict" directive switvhes the engine to the "modern" mode, changing the behavior of some built-in features.
+
+// Variables are used to store this info
+
+// A variable is a "names storage" for Data.We can use variables to store data.
+
+// To create a vaiable in Javascript,we need to use the let or var keyword.
+
+var message
+
+// assignment operator =
+
+// We add a sting ''
+// A string in Javascript is a set of characters surrounded by quatation marks.
+
+var message = 'Good morning';
+    //  message='How are you today?';
+alert(message);
+
+var name ='Ashton',
+     age = '18',
+     sport = 'soccer',
+     address = 'Portlands',
+     collage = 'Life Choices Academy',
+     gender = 'male',
+     car = 'nuhaha';
+     console.log('My name is ' + name +' and i am ' + age + ' years old.'+ 'My favourite sport is ' + sport +'.I am from a suburb in Mitchells plain called ' + address +'.I am doing a coding course at ' + collage +'.My gender is ' + gender +'.'+'I travel with 4 people to college but there is this person that always makes us late and her name is ' + car +'.')
+// ******************************************************
+// ***********************************************************
+
+var name = prompt("Enter your first name ");
+alert("Hello," + name);
+// Always remember to close a string with the same type of quatation marks you started it with.
+
+
+// Operators:
+
+// + addition
+// - substration
+// / division
+// * multiplication
+
+// the + operators can  be applied to two string values.This  will join two strings together, otherwise called concatenation.
+
+//In the console "Hey" + " there."
+
+//Choose meaningful names
+//Don't use reserved words
+//Follow conventions
+
+/* https:
+//developer.mozilla.org/en-US/docs/Web/JavaScripts/Reference/Lexical_grammar#Keywords */
+
+
+// Up until now, all the code in our programs has been executed chronologically.Let's enrich our code by adding condotional execution!
+// What's a condition?
